e91420459a4ec5c5c73e099ee46c933cc736fcde
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-02-13 10:05:00 +0100

    styled infopackage components

--- src/components/tabel.tsx
+++ src/components/tabel.tsx
@@ -3,42 +3,44 @@
 import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
 
-export const Tabel = ({ content, lang }: any) => {
-  const text =
-    lang === "sv" ? "Läs mer" : lang === "en" ? "Read more" : "Læs mere";
-
-  const text2 =
-    lang === "sv" ? "Läs mindre" : lang === "en" ? "Read less" : "Læs mindre";
-
+export const Tabel = ({ content }: any) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
     setIsOpen((prevState) => !prevState);
   };
+  console.log("conteeent", content);
 
   return (
-    <div className={`${content.length > 0 ? "visible mb-10" : "invisible"}`}>
-      <table>
-        <tbody>
-          {content?.map((row: any, index: number) => (
-            <tr
-              key={index}
-              style={{ display: index < 5 || isOpen ? "table-row" : "none" }}
-            >
-              <td>{row.country}</td>
-              <td>{row.dates}</td>
-              <td>{row.month}</td>
-              <td>{row.serie}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <div className="mb-4 font-bold">{content.subtitle}</div>
       <div
-        className="flex items-center cursor-pointer pt-4"
-        onClick={handleOpen}
+        className={`${
+          content?.tabel?.length > 0 ? "visible mb-10" : "invisible"
+        }`}
       >
-        {isOpen ? text : text2}
-        <IoIosArrowDown className={`ml-2 ${isOpen && "rotate-180"}`} />
+        <table>
+          <tbody>
+            {content?.tabel?.map((row: any, index: number) => (
+              <tr
+                key={index}
+                style={{ display: index < 5 || isOpen ? "table-row" : "none" }}
+              >
+                <td>{row.country}</td>
+                <td>{row.dates}</td>
+                <td>{row.month}</td>
+                <td>{row.serie}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div
+          className="flex items-center cursor-pointer pt-4"
+          onClick={handleOpen}
+        >
+          {isOpen ? "Läs mindre" : "Läs mer"}
+          <IoIosArrowDown className={`ml-2 ${isOpen && "rotate-180"}`} />
+        </div>
       </div>
     </div>
   );
